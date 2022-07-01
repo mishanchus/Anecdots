62054aab3e7ba2b27e5d9982777766e6c71fcee3
mishanchus <misha95ivanov@gmail.com>
2022-07-01 19:34:46 +0300

    2 commit

--- anecdots/urls.py
+++ anecdots/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import home_page, anec_by_cat, add_anec
+from .views import HomePageView, AnecByCat, AddAnec
 
 urlpatterns = [
-
-    path('', home_page, name = 'home'),
-    path('category/<int:category_id>/', anec_by_cat ,name = 'cats'),
-    path('new_anec/',add_anec, name = 'add_anec')
+    path('', HomePageView.as_view(), name='home'),
+    # path('', home_page, name = 'home'),
+    path('category/<int:category_id>/', AnecByCat.as_view() ,name = 'cats'),
+    # path('category/<int:category_id>/', anec_by_cat ,name = 'cats'),
+    path('new_anec/',AddAnec.as_view(), name = 'add_anec'),
+    # path('new_anec/',add_anec, name = 'add_anec')
 
 ]
--- anecdots/views.py
+++ anecdots/views.py
@@ -1,35 +1,54 @@
 from django.shortcuts import render, HttpResponse, redirect
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Anecdot, Category
 from .forms import AnecForm
+from django.core.paginator import Paginator
+from django.urls import reverse_lazy
+
+class HomePageView(ListView):
+    model = Anecdot
+    template_name = 'home.html'
+    context_object_name = 'anecs'
+    def get_queryset(self):
+        return Anecdot.objects.order_by('-pub_date')
+
+class AnecByCat(ListView):
+    model = Anecdot
+    template_name = 'home.html'
+    context_object_name = 'anecs'
+    def get_queryset(self):
+        return Anecdot.objects.order_by('-pub_date').filter(category=self.kwargs['category_id'])
 
 
-def home_page(request):
-    # categories = Category.objects.all()
-    anecs = Anecdot.objects.order_by('-pub_date')
-    context = {
-        # 'categories': categories,
-        'anecs': anecs
-    }
-    return render(request,'home.html', context)
+class AddAnec(CreateView):
+    form_class = AnecForm
+    template_name = 'add_anec.html'
+    success_url = reverse_lazy('home')
+# def add_anec(request):
+#     if request.method == 'POST':
+#         form = AnecForm(request.POST)
+#         if form.is_valid():
+# #            Anecdot.objects.create(**form.cleaned_data)
+#             form.save()
+#             return redirect('home')
+#     else:
+#         form = AnecForm()
+#     return render(request, 'add_anec.html', {'form':form})
 
 
-def anec_by_cat(request,category_id):
-    anecs = Anecdot.objects.order_by('-pub_date').filter(category=category_id)
-    # cats = Category.objects.all()
-    context = {
-        # 'categories':cats,
-        "anecs": anecs}
-    return render(request,'home.html', context)
+# def home_page(request):
+#     # categories = Category.objects.all()
+#     anecs = Anecdot.objects.order_by('-pub_date')
+#     context = {
+#         # 'categories': categories,
+#         'anecs': anecs
+#     }
+#     return render(request,'home.html', context)
 
-
-def add_anec(request):
-    if request.method == 'POST':
-        form = AnecForm(request.POST)
-        if form.is_valid():
-#            Anecdot.objects.create(**form.cleaned_data)
-            form.save()
-            return redirect('home')
-    else:
-        form = AnecForm()
-    return render(request, 'add_anec.html', {'form':form})
+# def anec_by_cat(request,category_id):
+#     anecs = Anecdot.objects.order_by('-pub_date').filter(category=category_id)
+#     # cats = Category.objects.all()
+#     context = {
+#         # 'categories':cats,
+#         "anecs": anecs}
+#     return render(request,'home.html', context)
